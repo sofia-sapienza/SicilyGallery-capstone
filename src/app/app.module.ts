b127d777d77bf1c414af1662f94bc92d5fbfe41b
Sofia Sapienza <sofiasapienza99@libero.it>
2023-06-19 14:44:46 +0200

    adds index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { EatComponent } from './components/eat/eat.component';
+import { PlayComponent } from './components/play/play.component';
+import { RelaxComponent } from './components/relax/relax.component';
+import {Route, RouterModule} from '@angular/router'; // importo il modulo {Route, RouterModule} per gestire le rotte
+
+
+const routes: Route[] = [ //creo una costante di :tipo Route per definire le rotte
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'eat',
+    component: EatComponent,
+  },
+  {
+    path: 'play',
+    component: PlayComponent,
+  },
+  {
+    path: 'relax',
+    component: RelaxComponent,
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    EatComponent,
+    PlayComponent,
+    RelaxComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes) // dichiaro negli imports che sto utilizzando il RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/relax/relax.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-relax',
+  templateUrl: './relax.component.html',
+  styleUrls: ['./relax.component.scss']
+})
+export class RelaxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
